1c3122d337a12113807620bd41040ecd4e91667d
Praveen Kumar <praveenscience@gmail.com>
2020-12-05 17:08:17 +0000

    Implemented morgan middleware.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const root = require("./routes/root");
 const app = express();
 const port = 5000;
 
+app.use(morgan("dev"));
 app.use("/", root);
 
 app.listen(port, () => {
