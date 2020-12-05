1ff0524f8d7ba3609708545e77d4a46dec3860a1
Praveen Kumar <praveenscience@gmail.com>
2020-12-05 16:54:29 +0000

    Implemented ToDo!

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -5,5 +5,13 @@ const app = express.Router();
 app.get("/", (req, res) => {
   res.json(todos);
 });
+app.get("/:id", (req, res) => {
+  const id = +req.params.id;
+  if (todos[id]) {
+    res.json(todos[id]);
+  } else {
+    res.status(404).json("Error! Todo Not Found!");
+  }
+});
 
 module.exports = app;
