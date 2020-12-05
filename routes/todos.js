0cc124a11a53b0e2880ec19151e52bd9ab945ebe
Praveen Kumar <praveenscience@gmail.com>
2020-12-05 16:50:03 +0000

    Created modular routing for Root, Users and ToDos.

--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const api = require("./api");
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  res.json("Welcome to GeeksforGeeks API Server!");
+  res.json("Welcome to ToDos!");
 });
-app.use("/api", api);
 
 module.exports = app;
