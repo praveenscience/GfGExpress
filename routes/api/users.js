5d0384a38432df19e6a8df3064b102efb7984e1e
Praveen Kumar <praveenscience@gmail.com>
2020-12-05 17:01:35 +0000

    Implemented single user with removing & adding fields.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,23 @@ const users = require("../../constants/users");
 const app = express.Router();
 
 app.get("/", (req, res) => {
-  res.json(users);
+  res.json(
+    users.map((u, UserID) => {
+      const a = { UserID, ...u };
+      delete a.Password;
+      return a;
+    })
+  );
+});
+app.get("/:id", (req, res) => {
+  const UserID = +req.params.id;
+  if (users[UserID]) {
+    const a = { UserID, ...users[UserID] };
+    delete a.Password;
+    res.json(a);
+  } else {
+    res.status(404).json("Error! User Not Found!");
+  }
 });
 
 module.exports = app;
