db445ff2cd5f729211c36e0c1da928d4958a5699
Praveen Kumar <praveenscience@gmail.com>
2020-12-05 17:33:50 +0000

    Updated users.

--- constants/users.js
+++ constants/users.js
@@ -3,40 +3,5 @@ module.exports = [
     Name: "Praveen Kumar",
     Password: "Hello@123",
     LinkedIn: "https://www.linkedin.com/in/praveentech/"
-  },
-  {
-    Name: "Ashish Sharma",
-    Password: "test@123",
-    LinkedIn: "http://in.linkedin.com/in/ashish1sharma"
-  },
-  {
-    Name: "Isabel Achurra",
-    Password: "Kitten@666",
-    LinkedIn: "https://www.linkedin.com/in/freederth/"
-  },
-  {
-    Name: "Shivam Kaushik",
-    Password: "!@#$%^&*()",
-    LinkedIn: "https://www.linkedin.com/in/shivam-kaushik-67b533148/"
-  },
-  {
-    Name: "Sanskar",
-    Password: "Yooo",
-    LinkedIn: "https://www.linkedin.com/in/sanskar-bajaj-68b94216b/"
-  },
-  {
-    Name: "Himadri Bhargava",
-    Password: "himadri@123",
-    LinkedIn: "https://www.linkedin.com/in/himadribhargava/"
-  },
-  {
-    Name: "Raju",
-    Password: "pass@123",
-    LinkedIn: "https://www.linkedin.com/in/raju-pokiya-47a24115/"
-  },
-  {
-    Name: "Maneet Kaur",
-    Password: "@123",
-    Linkedln: "www.linkedin.com/in/maneet499"
   }
 ];
