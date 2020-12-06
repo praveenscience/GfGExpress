b85f9333569016f320c5e8cdfb07b1a2a84639fb
Praveen Kumar <praveenscience@gmail.com>
2020-12-06 15:05:20 +0000

    Created two dummy users with passwords.

--- constants/users.js
+++ constants/users.js
@@ -1,7 +1,14 @@
 module.exports = [
   {
-    Name: "Praveen Kumar",
+    Username: "Praveen",
     Password: "Hello@123",
+    Name: "Praveen Kumar",
     LinkedIn: "https://www.linkedin.com/in/praveentech/"
+  },
+  {
+    Username: "John",
+    Password: "Corona@123",
+    Name: "John Corona",
+    LinkedIn: "https://www.linkedin.com/in/covid19/"
   }
 ];
